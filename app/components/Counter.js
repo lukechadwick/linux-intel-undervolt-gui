83fa31feda9bdfcce7b4243c9d9f383088aa126b
luke <llukechadwick@gmail.com>
2019-02-05 17:08:46 +1300

    reading voltage results to state

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -4,21 +4,53 @@ import { Link } from 'react-router-dom';
 import styles from './Counter.css';
 import routes from '../constants/routes';
 
+var sudo = require('sudo-js');
+let pass = '';
+
 export default class Counter extends Component<Props> {
   constructor(props) {
     super(props);
 
     this.state = {
-      voltage: 0
+      coreCacheValue: 0,
+      gpuVoltage: 0,
+      uncoreVoltage: 0,
+      analogioVoltage: 0,
+      password: ''
     };
   }
 
+  readVoltage = () => {
+    sudo.setPassword(this.state.password);
+
+    var command = [
+      'python',
+      '/home/luke/workspace/undervolt-gui/undervolt/undervolt.py',
+      '-r'
+    ];
+
+    sudo.exec(command, (err, pid, result) => {
+      let formattedArray = [];
+      let resultArray = result.split('\n'); //Split result into array by line
+
+      resultArray.forEach(element => {
+        console.log(element);
+        formattedArray.push(element.replace(/[^\d\.]*/g, '')); //Remove text/strings from voltage numbers
+      });
+
+      formattedArray = formattedArray.filter(Boolean); //Remove blank lines
+      this.setState({
+        coreCacheValue: '-' + formattedArray[1],
+        gpuVoltage: '-' + formattedArray[2],
+        uncoreVoltage: '-' + formattedArray[3],
+        analogioVoltage: '-' + formattedArray[4]
+      });
+    });
+  };
+
   handleInputChange = e => {
     console.log(event.target.value);
-
-    this.setState({ voltage: event.target.value });
-
-    // /output.innerHTML = this.value;
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
@@ -32,27 +64,61 @@ export default class Counter extends Component<Props> {
         <br />
         <br />
 
-        <h1>Custom Range Slider</h1>
+        <h1>Undervolt Settings</h1>
+        <span>Root Password </span>
 
-        <div class="slidecontainer">
-          <p>Default range slider:</p>
+        <br />
+        <input
+          name="password"
+          type="password"
+          onChange={e => this.handleInputChange(e)}
+        />
+
+        <button onClick={() => this.readVoltage()}>Read</button>
+        <div className="slidecontainer">
+          <p>Core/Cache Voltage:</p>
           <input
+            name="coreCacheValue"
             type="range"
             min="-250"
             max="0"
-            value={this.state.voltage}
+            value={this.state.coreCacheValue}
             onChange={e => this.handleInputChange(e)}
           />
+          <span> {this.state.coreCacheValue} mv</span>
 
-          <p>Custom range slider:</p>
+          <p>GPU Voltage:</p>
           <input
+            name="gpuVoltage"
             type="range"
-            min="1"
-            max="100"
-            value="50"
-            class="slider"
-            id="myRange"
+            min="-250"
+            max="0"
+            value={this.state.gpuVoltage}
+            onChange={e => this.handleInputChange(e)}
           />
+          <span> {this.state.gpuVoltage} mv</span>
+
+          <p>Uncore Voltage:</p>
+          <input
+            name="uncoreVoltage"
+            type="range"
+            min="-250"
+            max="0"
+            value={this.state.uncoreVoltage}
+            onChange={e => this.handleInputChange(e)}
+          />
+          <span> {this.state.uncoreVoltage} mv</span>
+
+          <p>Analog IO Voltage:</p>
+          <input
+            name="analogioVoltage"
+            type="range"
+            min="-250"
+            max="0"
+            value={this.state.analogioVoltage}
+            onChange={e => this.handleInputChange(e)}
+          />
+          <span> {this.state.analogioVoltage} mv</span>
         </div>
 
         <div className={styles.btnGroup} />
--- app/components/Home.js
+++ app/components/Home.js
@@ -4,37 +4,12 @@ import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
 
-var sudo = require('sudo-js');
-let pass = '';
-
-type Props = {};
-
-export default class Home extends Component<Props> {
-  props: Props;
-
-  readVoltage = () => {
-    var command = [
-      'python',
-      '/home/luke/workspace/undervolt-gui/undervolt/undervolt.py',
-      '-r'
-    ];
-
-    sudo.exec(command, function(err, pid, result) {
-      console.log(result);
-    });
-  };
-
-  handleInputChange = event => {
-    sudo.setPassword(event.target.value);
-  };
-
+export default class Home extends Component {
   render() {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-        <button onClick={() => this.readVoltage()}>Read</button>
-        <input type="password" onChange={e => this.handleInputChange(e)} />
+        <Link to={routes.COUNTER}>to Volage Control</Link>
       </div>
     );
   }
