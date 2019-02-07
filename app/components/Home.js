cefb590baf6d52497b102ee473bc25bbf81f30df
luke <llukechadwick@gmail.com>
2019-02-07 15:57:59 +1300

    add cpu stress bench

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,12 +4,42 @@ import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
 
+var path = require('path');
+var cluster = require('cluster');
+
+cluster.setupMaster({
+  exec: path.join(__dirname, 'cpuBench.js'),
+  //args: ['--use', 'https'],
+  silent: false
+});
+
 export default class Home extends Component {
+  cpuBench = () => {
+    if (cluster.isMaster) {
+      // Count the machine's CPUs
+      var cpuCount = require('os').cpus().length;
+
+      // Create a worker for each CPU
+      for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+      }
+      // Code to run if we're in a worker process
+    }
+  };
+
+  endBench = () => {
+    console.log(cluster.workers);
+
+    //cluster.workers.kill();
+  };
   render() {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Voltage Control</Link>
+        <br />
+        <button onClick={this.cpuBench}>bench</button>
+        <button onClick={this.endBench}>stop bench</button>
       </div>
     );
   }
---
+++ app/cpuBench.js
@@ -0,0 +1,5 @@
+const { fib, dist } = require('cpu-benchmark');
+console.log('doof');
+
+//console.log(fib(2000));
+console.log(dist(10000));
